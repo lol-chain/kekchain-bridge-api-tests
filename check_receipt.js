6451209d622bccf0ebbb37223dd6c3f632ec6706
root <root@vmi998913.contaboserver.net>
2022-09-25 03:30:40 +0200

    kekpoint

--- check_receipt.js
+++ check_receipt.js
@@ -5,12 +5,12 @@ async function doPostRequest() {
     //let data = { method: 'log_tx', address: '0xC925F19cb5f22F936524D2E8b17332a6f4338751', hash: '0xc8d58343bc2e8214b444a6ab95756388448ac1cad8814bd412a17fd15adff514' };
     
     let res = await axios.post('https://bridge-api.interchained.org/api.php', querystring.stringify({
-            method: 'tx_log_update', //gave the values directly for testing
+            method: 'check_receipt', //gave the values directly for testing
 	    path: 'KEK-ETH',
 	    height: 303880,
 	    status: "CONFIRMED",
             address: '0x050134fd4EA6547846EdE4C4Bf46A334B7e87cCD',
-            hash: '0xf5b5c9baf3a18412c0ffd91a98feeeed3314c0c72d6b181374abe3b1a2100037' 
+            hash: '0xe0f47c4aee2cfa3a125d26403b055b882668e9da0ee0176b3e24e757697fd8de' 
     }), {
       headers: { 
         "Content-Type": "application/x-www-form-urlencoded"
--- local_hash_bulk.js
+++ local_hash_bulk.js
@@ -5,12 +5,13 @@ async function doPostRequest() {
     //let data = { method: 'log_tx', address: '0xC925F19cb5f22F936524D2E8b17332a6f4338751', hash: '0xc8d58343bc2e8214b444a6ab95756388448ac1cad8814bd412a17fd15adff514' };
     
     let res = await axios.post('https://bridge-api.interchained.org/api.php', querystring.stringify({
-            method: 'tx_log_update', //gave the values directly for testing
+            method: 'local_hash', //gave the values directly for testing
 	    path: 'KEK-ETH',
-	    height: 303880,
+	    type: 'CONFIRMED',
+	    height: 304285,
 	    status: "CONFIRMED",
             address: '0x050134fd4EA6547846EdE4C4Bf46A334B7e87cCD',
-            hash: '0xf5b5c9baf3a18412c0ffd91a98feeeed3314c0c72d6b181374abe3b1a2100037' 
+            hash: '0x288a42a159b56da316d48c5501228941193928e07311eb4ff33ec7cd0e2bdd5f' 
     }), {
       headers: { 
         "Content-Type": "application/x-www-form-urlencoded"
