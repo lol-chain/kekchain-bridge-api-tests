9b697a3c4575220048ed3cfbb5aff26db3176a78
root <root@vmi998913.contaboserver.net>
2022-09-28 15:22:29 +0200

    kekpoint: fee added

--- get_fee_kek.js
+++ get_fee_kek.js
@@ -5,12 +5,13 @@ async function doPostRequest() {
     //let data = { method: 'log_tx', address: '0xC925F19cb5f22F936524D2E8b17332a6f4338751', hash: '0xc8d58343bc2e8214b444a6ab95756388448ac1cad8814bd412a17fd15adff514' };
     
     let res = await axios.post('https://bridge-api.interchained.org/api.php', querystring.stringify({
-            method: 'local_hash_stats', //gave the values directly for testing
+            method: 'min_fee_kek', //gave the values directly for testing
 	    path: 'ETH-KEK',
-	    height: 304285,
-	    status: "CONFIRMED",
-            address: '0x050134fd4EA6547846EdE4C4Bf46A334B7e87cCD',
-            hash: '0x07ff6fc19650d59b5274ac7ff47418d66473678c7dda9e8ebe4b39a701a12377' 
+	    app_spec_key: '0x3215614be',
+            currency: 'ETH',
+	    amount: "50000000.00",
+	    address: '0x050134fd4ea6547846ede4c4bf46a334b7e87ccd',
+            hash: '0x64a010f7a37726ea931064f5b848112dce7c1f6886751b72a30bba84ab6502a0' 
     }), {
       headers: { 
         "Content-Type": "application/x-www-form-urlencoded"
--- mark_paid.js
+++ mark_paid.js
@@ -4,13 +4,13 @@ async function doPostRequest() {
 
     //let data = { method: 'log_tx', address: '0xC925F19cb5f22F936524D2E8b17332a6f4338751', hash: '0xc8d58343bc2e8214b444a6ab95756388448ac1cad8814bd412a17fd15adff514' };
     
-    let res = await axios.post('https://bridge-api.interchained.org/api.php', querystring.stringify({
-            method: 'local_hash_stats', //gave the values directly for testing
+    let res = await axios.post('https://bridge-api.interchained.org/adminapi.php', querystring.stringify({
+            method: 'mark_paid', //gave the values directly for testing
 	    path: 'ETH-KEK',
-	    height: 304285,
-	    status: "CONFIRMED",
-            address: '0x050134fd4EA6547846EdE4C4Bf46A334B7e87cCD',
-            hash: '0x07ff6fc19650d59b5274ac7ff47418d66473678c7dda9e8ebe4b39a701a12377' 
+	    app_spec_key: '0x103090mm031291',
+            currency: 'ETH',
+	    address: '0x3215614be56c79511fc87b3e082cffcf2cd5d79e',
+            hash: '0xdc27be404deb680d46555e9d12609329347e98e52d95c38e66db00d0ed3bbe8e' 
     }), {
       headers: { 
         "Content-Type": "application/x-www-form-urlencoded"
